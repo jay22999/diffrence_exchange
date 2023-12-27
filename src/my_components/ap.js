6039ae1568273b12d088f95d192ee9fd16e4e5f8
jay22999 <57299784+jay22999@users.noreply.github.com>
2023-12-27 15:25:33 +0530

    Update ap.js

--- src/my_components/ap.js
+++ src/my_components/ap.js
@@ -6,7 +6,7 @@ import { ungzip } from "pako";
 //   transports: ["websocket"],
 // });
 
-const sio = io("https://arbi-cryon92.ddns.net", {
+const sio = io("http://arbi-cryon92.ddns.net", {
   transports: ["websocket"],
 });
 
